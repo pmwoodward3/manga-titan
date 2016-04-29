d5de946b6732a1b1732b36bcde3b35c70deff421
nothing628 <yogyphang@gmail.com>
2016-04-29 17:29:58 +0700

    Remove unused component

--- modules/Manga/Assets/main.js
+++ modules/Manga/Assets/main.js
@@ -1,11 +1,8 @@
-Vue.component('manga-list',			require('./component/manga-list.vue'));
-Vue.component('manga-read',			require('./component/manga-read.vue'));
-Vue.component('manga-thumb',		require('./component/manga-thumb.vue'));
 Vue.component('vue-filter',			require('./component/vue-filter.vue'));
 Vue.component('vue-pagination',		require('./component/vue-pagination.vue'));
 Vue.component('vue-list',			require('./component/vue-list.vue'));
 Vue.component('vue-navigator',		require('./component/vue-navigator.vue'));
-Vue.component('vue-image',			require('./component/vue-image.vue'));
+Vue.component('vue-manga-page',		require('./component/vue-manga-page.vue'));
 Vue.component('vue-extra-content',	require('./component/vue-extra-content.vue'));
 Vue.component('vue-desc',			require('./component/vue-desc.vue'));
 Vue.component('vue-desc-image',		require('./component/vue-desc-image.vue'));
