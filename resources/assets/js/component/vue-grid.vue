31a4cea8dd72a8287db9a15eb5b4f8d231234141
nothing628 <yogyphang@gmail.com>
2016-05-02 16:01:01 +0700

    New component

---
+++ resources/assets/js/component/vue-grid.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="ui list"></div>
+</template>
+
+<script>
+	module.exports = {
+		props: {
+			maps:	{ required: true, type: Array }
+		},
+		data: function () {
+			return {
+				items: []
+			}
+		},
+		events: {
+			'row-flash': function (data) {
+				this.items = data;
+			}
+		},
+		ready: function () {
+			//
+		}
+	}
+</script>
